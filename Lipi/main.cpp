8c1def5b9f02e4650f732463cca8e02bb2639cad
NishanBhattarai327 <bhattarainishan327@gmail.com>
2023-08-13 08:10:48 +0545

    Add Editor class

--- Lipi/main.cpp
+++ Lipi/main.cpp
@@ -2,28 +2,84 @@
 #include <SFML/System/Clock.hpp>
 #include <iostream>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(1080, 720), "Lipi");
-
-    // Create a graphical text to display
+class Editor {
     sf::Font font;
-    if (!font.loadFromFile("res/fonts/arial.ttf"))
-        return EXIT_FAILURE;
-    sf::Text text("", font, 24);
-    std::string buff = "";
+    std::string s_textbuff;
+    sf::RectangleShape cursor;
+    float cursorYOffset;
+    float cursorXOffset;
+    float cursorX;
+    float cursorY;
+public: 
+    sf::Text sfml_text;
 
-    sf::RectangleShape cursor(sf::Vector2f(1.25f, 24.f));
-    float cursorYOffset = 24.f - 20.f;
-    float cursorXOffset = 24.f - 23.f;
-    float cursorX = text.findCharacterPos(buff.length()).x;
-    float cursorY = text.findCharacterPos(buff.length()).y + cursorYOffset;
-    cursor.setPosition(cursorX, cursorY);
+    Editor() {
+        if (!font.loadFromFile("res/fonts/arial.ttf")) {
+            std::cout << "ERROR:: Cannot open font" << std::endl;
+            return;
+        }
 
-    
+        sfml_text.setFont(font);
+        sfml_text.setCharacterSize(24);
 
+        cursor.setSize(sf::Vector2(1.25f, 24.f));
+        cursorYOffset = 24.f - 20.f;
+        cursorXOffset = 24.f - 23.f;
+        cursorX = 0;
+        cursorY = 0;
+        cursor.setPosition(cursorX, cursorY);
+    }
+
+    void updateText(sf::Uint32 unicode) {
+        if (unicode >= 32 && unicode < 127) {
+            s_textbuff += unicode;
+            sfml_text.setString(s_textbuff);
+        }
+        else if (unicode == 8 && !s_textbuff.empty()) {
+            s_textbuff.pop_back();
+            sfml_text.setString(s_textbuff);
+        }
+        else if (unicode == 13) {
+            s_textbuff += "\n";
+            sfml_text.setString(s_textbuff);
+        }
+    }
+
+    void updateCursor(float x = -1.f, float y = -1.f) {
+        if (x == -1.f)
+            cursorX = sfml_text.findCharacterPos(s_textbuff.length()).x + cursorXOffset;
+        if (x == -1.f)
+            cursorY = sfml_text.findCharacterPos(s_textbuff.length()).y + cursorYOffset;
+
+        if (x != -1.f && y != -1.f) {
+            cursorX = x + cursorXOffset;
+            cursorY = y + cursorYOffset;
+        }
+        cursor.setPosition(cursorX, cursorY);
+    }
+
+    void draw(sf::RenderWindow& window, float& cursorBlinkTime) {
+        // Draw the string
+        window.draw(sfml_text);
+
+        // Blink the cursor
+        if (cursorBlinkTime > 0.5f) {
+            window.draw(cursor);
+        }
+
+        if (cursorBlinkTime > 1.f) {
+            cursorBlinkTime = 0.f;
+        }
+    }
+};
+
+void run() {
+    sf::RenderWindow window(sf::VideoMode(1080, 720), "Lipi");
+   
     sf::Clock clock;
     float cursorBlinkTime = 0.f;
+
+    Editor editor;
 
     while (window.isOpen())
     {
@@ -37,49 +93,22 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
             if (event.type == sf::Event::TextEntered) {
-
-                if (event.text.unicode >= 32 && event.text.unicode < 127) {
-                    buff += event.text.unicode;
-                    text.setString(buff);
-                    cursorX = text.findCharacterPos(buff.length()).x + cursorXOffset;
-                    cursorY = text.findCharacterPos(buff.length()).y + cursorYOffset;
-                    cursor.setPosition(cursorX, cursorY);
-                }
-                else if (event.text.unicode == 8) {
-                    if (!buff.empty()) {
-                        buff.pop_back();
-                        text.setString(buff);
-                        cursorX = text.findCharacterPos(buff.length()).x + cursorXOffset;
-                        cursorY = text.findCharacterPos(buff.length()).y + cursorYOffset;
-                    }
-                    cursor.setPosition(cursorX, cursorY);
-                }
-                else if (event.text.unicode == 13) {
-                    buff += "\n";
-                    text.setString(buff);
-                    cursorX = text.findCharacterPos(buff.length()).x + cursorXOffset;
-                    cursorY = text.findCharacterPos(buff.length()).y + cursorYOffset;
-                    cursor.setPosition(cursorX, cursorY);
-                }
+                editor.updateText(event.text.unicode);
+                editor.updateCursor();
             }
         }
 
         window.clear();
 
-        // Draw the string
-        window.draw(text);
-        
-        // Blink the cursor
-        if (cursorBlinkTime  > 0.5f) {
-            window.draw(cursor);
-        }
-        
-        if (cursorBlinkTime > 1.f) {
-            cursorBlinkTime = 0.f;
-        }
+        editor.draw(window, cursorBlinkTime);
         
         window.display();
     }
+}
+
+int main()
+{
+    run();
 
     return 0;
 }
